805ae1c0b01936fa2d1c559cb3a7f333eb7ff316
Olivier van Helden <olivier@van-helden.net>
2025-09-16 15:43:14 +0200

    clean lib/agnstk to setup git subrepo

--- lib/agnstk/bootstrap/providers.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return [
-    Agnstk\Providers\AppServiceProvider::class,
-];
--- lib/agnstk/resources/views/sections/content-markdown.blade.php
+++
@@ -1,4 +0,0 @@
-{{-- Markdown content section with proper styling --}}
-<div class="markdown-content">
-    {!! $content !!}
-</div>
